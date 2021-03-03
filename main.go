9f60f0cc395fd5fa6e57906165ccb6930fbeb07a
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-03-03 13:24:34 +0900

    Add SVNUser

--- main.go
+++ main.go
@@ -102,6 +102,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "SVNRepository")
 		os.Exit(1)
 	}
+	if err = (&controllers.SVNUserReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("SVNUser"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SVNUser")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
