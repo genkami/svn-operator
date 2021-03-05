ea10a705ab81d1080d4809985c44525868e04a0c
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-03-06 03:28:00 +0900

    Update svn-server to use server-updater

--- docker/svn/entrypoint.sh
+++ docker/svn/entrypoint.sh
@@ -12,6 +12,7 @@ export SERVER_NAME="test.example.com"
 mkdir -p /svn
 chown -R www-data:www-data /svn
 
-sudo -u www-data -g www-data /work/initialize-repo.sh
+sudo -u www-data -g www-data mkdir -p /svn/repos
+sudo -u www-data -g www-data /work/server-updater &
 
 exec apache2 -DFOREGROUND "$@"
