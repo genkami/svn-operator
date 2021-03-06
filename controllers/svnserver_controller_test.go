172082c8a90bf16e887c59433ee18f2dacea65f2
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-03-07 03:24:57 +0900

    Test: ensure that .Spec.PodTemplate.Image works

--- controllers/svnserver_controller_test.go
+++ controllers/svnserver_controller_test.go
@@ -37,39 +37,45 @@ var _ = Describe("SVNServer Controller", func() {
 		SVNServerNamespace = "default"
 
 		timeout  = 10 * time.Second
-		duration = 10 * time.Second
 		interval = 250 * time.Millisecond
 	)
+
+	defaultSVNServer := func() *svnv1alpha1.SVNServer {
+		return &svnv1alpha1.SVNServer{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "svn.k8s.oyasumi.club/v1alpha1",
+				Kind:       "SVNServer",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      SVNServerName,
+				Namespace: SVNServerNamespace,
+			},
+			Spec: svnv1alpha1.SVNServerSpec{
+				VolumeClaimTemplate: corev1.PersistentVolumeClaim{
+					Spec: corev1.PersistentVolumeClaimSpec{
+						AccessModes: []corev1.PersistentVolumeAccessMode{
+							corev1.ReadWriteOnce,
+						},
+						Resources: corev1.ResourceRequirements{
+							Requests: map[corev1.ResourceName]resource.Quantity{
+								corev1.ResourceStorage: resource.MustParse("1G"),
+							},
+						},
+					},
+				},
+			},
+		}
+	}
 
 	Context("When SVNServer Status is created", func() {
 		It("Creates corresponding resources automatically", func() {
 			By("creating a new SVNServer")
 			ctx := context.Background()
-			svnServer := &svnv1alpha1.SVNServer{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "svn.k8s.oyasumi.club/v1alpha1",
-					Kind:       "SVNServer",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      SVNServerName,
-					Namespace: SVNServerNamespace,
-				},
-				Spec: svnv1alpha1.SVNServerSpec{
-					VolumeClaimTemplate: corev1.PersistentVolumeClaim{
-						Spec: corev1.PersistentVolumeClaimSpec{
-							AccessModes: []corev1.PersistentVolumeAccessMode{
-								corev1.ReadWriteOnce,
-							},
-							Resources: corev1.ResourceRequirements{
-								Requests: map[corev1.ResourceName]resource.Quantity{
-									corev1.ResourceStorage: resource.MustParse("1G"),
-								},
-							},
-						},
-					},
-				},
-			}
+			svnServer := defaultSVNServer()
 			Expect(k8sClient.Create(ctx, svnServer)).To(Succeed())
+			defer func() {
+				Expect(k8sClient.Delete(ctx, svnServer)).To(Succeed())
+			}()
 
 			By("checking whether the StatefulSet is created")
 			statefulSetLookupKey := types.NamespacedName{Name: SVNServerName, Namespace: SVNServerNamespace}
@@ -78,6 +84,9 @@ var _ = Describe("SVNServer Controller", func() {
 				err := k8sClient.Get(ctx, statefulSetLookupKey, statefulSet)
 				return err == nil
 			}, timeout, interval).Should(BeTrue())
+			defer func() {
+				Expect(k8sClient.Delete(ctx, statefulSet)).To(Succeed())
+			}()
 
 			By("checking whether SVNServer.Status.Conditions is updated")
 			svnServerLookupKey := types.NamespacedName{Name: SVNServerName, Namespace: SVNServerNamespace}
@@ -91,6 +100,56 @@ var _ = Describe("SVNServer Controller", func() {
 			}, timeout, interval).Should(BeNumerically(">=", 1))
 			conds := createdSVNServer.Status.Conditions
 			Expect(conds[len(conds)-1].Type).To(Equal(svnv1alpha1.ConditionTypeSynced))
+		})
+	})
+
+	Describe(".Spec.PodTemplate.Image", func() {
+		Context("when the field is not set", func() {
+			It("uses the default value", func() {
+				ctx := context.Background()
+				svnServer := defaultSVNServer()
+				Expect(k8sClient.Create(ctx, svnServer)).To(Succeed())
+				defer func() {
+					Expect(k8sClient.Delete(ctx, svnServer)).To(Succeed())
+				}()
+
+				statefulSetLookupKey := types.NamespacedName{Name: SVNServerName, Namespace: SVNServerNamespace}
+				statefulSet := &appsv1.StatefulSet{}
+				Eventually(func() bool {
+					err := k8sClient.Get(ctx, statefulSetLookupKey, statefulSet)
+					return err == nil
+				}, timeout, interval).Should(BeTrue())
+				defer func() {
+					Expect(k8sClient.Delete(ctx, statefulSet)).To(Succeed())
+				}()
+
+				Expect(statefulSet.Spec.Template.Spec.Containers[0].Image).To(Equal(defaultSVNServerImageForTest))
+			})
+		})
+
+		Context("when the field is set", func() {
+			It("uses the given value", func() {
+				ctx := context.Background()
+				svnServer := defaultSVNServer()
+				svnServer.Spec.PodTemplate.Image = "my-image:latest"
+				Expect(k8sClient.Create(ctx, svnServer)).To(Succeed())
+				defer func() {
+					Expect(k8sClient.Delete(ctx, svnServer)).To(Succeed())
+				}()
+
+				statefulSetLookupKey := types.NamespacedName{Name: SVNServerName, Namespace: SVNServerNamespace}
+				statefulSet := &appsv1.StatefulSet{}
+				Eventually(func() (string, error) {
+					err := k8sClient.Get(ctx, statefulSetLookupKey, statefulSet)
+					if err != nil {
+						return "", err
+					}
+					return statefulSet.Spec.Template.Spec.Containers[0].Image, nil
+				}, timeout, interval).Should(Equal("my-image:latest"))
+				defer func() {
+					Expect(k8sClient.Delete(ctx, statefulSet)).To(Succeed())
+				}()
+			})
 		})
 	})
 })
